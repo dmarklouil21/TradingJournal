6e551ae75c42bd36b5bead7a627a7de396817e7c
Mark Louil Diacamos <dmarklouil@gmail.com>
2026-08-25 20:40:38 +0800

    added env config

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5234/api/"
+  baseURL: import.meta.env.API_BASE_URL
 });
 
 apiClient.interceptors.request.use(
